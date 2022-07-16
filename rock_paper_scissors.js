bb5562811acc2cd15d147cb98aba6005eba9ff6d
Colton Turner <colton_232@hotmail.com>
2022-07-16 14:24:44 -0700

    Created getComputerChoice function.

---
+++ rock_paper_scissors.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    num = Math.floor(Math.random() * 3);
+    if (num == 0) {
+        return "rock";
+    }
+    else if (num == 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
